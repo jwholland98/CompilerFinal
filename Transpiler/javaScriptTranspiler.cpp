0759a4ec3b9ec252dd1bd02cc73d2c3115db8ec9
J.D <jacobcline@protonmail.com>
2019-11-30 10:34:46 -0700

    finished ifelse()

--- Transpiler/javaScriptTranspiler.cpp
+++ Transpiler/javaScriptTranspiler.cpp
@@ -7,25 +7,14 @@ using namespace std;
 
 int main(){
 	Parser p;
-	string expression = 
-						//"#include <iostream>"
-						//"using namespace std;"
-						"int main(){"
+	string expression ="int main(){"
 							"int i=0;"
-							"cout << \"hi\" << i << endl << \"hi\";"
-							/*"for(int i=0;i<3;i=i+1){"
-								"int c=0;"
-							"}"
 							"if(i<3){"
 								"int y=0;"
 							"}else{"
 								"int b=0;"
-							"}"*/
-						"}"/*
-						
-						"int test(){"
-								"int x=5;"
-						"}"*/;
+							"}"
+						"}";
 	p.scan(expression);
 	summary();
 	return 0;
