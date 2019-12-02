876ad12cbbd9ba41b97c5b1d4e7276be0746edb5
J.D <jacobcline@protonmail.com>
2019-12-02 14:04:10 -0700

    just trying to push...

--- Transpiler/javaScriptTranspiler.cpp
+++ Transpiler/javaScriptTranspiler.cpp
@@ -8,11 +8,11 @@ using namespace std;
 int main(){
 	Parser p;
 	string expression ="int main(){"
-							"int i=0;"
+							"int i=0+3+i;"
 							"if(i<3){"
-								"int y=0;"
+								"int y=10;"
 							"}else{"
-								"int b=0;"
+								"int b=32;"
 							"}"
 						"}";
 	p.scan(expression);
--- Transpiler/parser.h
+++ Transpiler/parser.h
@@ -5,6 +5,8 @@
 #include <vector>
 #include <string>
 #include <iomanip>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
